55f509e0582c28f86a2128a51d738b4e65fd726f
Lynk229 <nguyenly10072002@gmail.com>
2023-10-02 19:08:01 +0700

    add new file

---
+++ Project/NewApp/Models/Student.cs
@@ -0,0 +1,13 @@
+namespace NewApp.Models{
+    public class Student{
+        public string MaSV {get; set;}
+        public string HoTen {get; set; }
+        public string DiaChi{get; set;}
+     
+       public void ThongTin(string MaSV, string HoTen, string DiaChi)
+    {
+         System.Console.WriteLine("{0} - {1} - {2} ", MaSV,HoTen, DiaChi);
+           }
+
+    }
+}
